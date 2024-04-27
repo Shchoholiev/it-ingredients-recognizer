33e84045888a361f42214acd019c9ae4bae1633b
Shchoholiev <sh.codework@gmail.com>
2024-04-27 20:45:38 +0000

    Add logic to capture images with camera - Add cameras package - Add app logger - Add requirements.txt - Update devcontainer.json to install packages on container start

---
+++ ingredients-recognizer/cameras/operations.py
@@ -0,0 +1,45 @@
+import subprocess
+import io
+from PIL import Image
+from app.logger import logger
+
+def capture_image():
+    """
+    Captures an image from the camera using libcamera-still and flips it 
+    because the camera is installed upside down.
+
+    Returns:
+        bytes: Image bytes.
+        
+    Raises:
+        Exception: If there is an error capturing the image.
+    """
+
+    try:
+        # File to save the captured image temporarily
+        temp_image_file = "/tmp/captured_image.jpg"
+        
+        # Capture image using libcamera-still
+        subprocess.run(["libcamera-still", "-o", temp_image_file, "-t", "100", "--nopreview", "--autofocus-mode=manual", "--lens-position=4"], check=True)
+
+        # Open the captured image
+        with open(temp_image_file, "rb") as image_file:
+            stream = io.BytesIO(image_file.read())
+        
+        # Flip the image horizontally
+        stream.seek(0)  # Go to the start of the stream
+        image = Image.open(stream)
+        flipped_image = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+        # Save the flipped image to a new stream
+        flipped_stream = io.BytesIO()
+        flipped_image.save(flipped_stream, format='jpeg')
+        flipped_image_bytes = flipped_stream.getvalue()
+
+        logger.info("Image captured and flipped.")
+
+        return flipped_image_bytes
+
+    except Exception as e:
+        logger.error(f"Error processing image: {e}")
+        return None
